e27529753ed2a115cc109598466e6d40ca54f056
jake.bower <jake.bower@4f06683a-ac53-0410-8745-075dec9ad3e4>
2008-08-06 01:15:44 +0000

    Added the common folder from the CUDA SDK and the rot13 source code first attempt.

---
+++ src/rot13/rot13.cu
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2008 Jacob Bower
+ * 
+ * This source is licensed under the GNU General Public License v2
+ */
+ 
+// includes, system
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+// includes, project
+#include <cutil.h>
+
+// includes, kernels
+#include <rot13Kern.cu>
+
+void rot13WholeFile(char* filename);
+
+int
+main(int argc, char** argv)
+{
+    // Read file from disk to memory
+	if(argc < 2)
+	{
+	  printf("Usage: rot13 inputfile.txt");
+	  CUT_EXIT(argc, argv);
+	}
+	
+	rot13OneLine(argv[1]);
+	
+    CUT_EXIT(argc, argv);
+}
+
+/* This function assumes file is less than 10K and all one line */
+void rot13OneLine(const char* filename)
+{
+  const size_t bufferSz = 10240;
+  // Allocate host memory
+  char* fileBuf = malloc(bufferSz);
+  
+  // Read file
+  FILE * h_file = fopen(filename, "r");
+  fgets(fileBuf, bufferSz - 1, h_file);
+  fclose(h_file);
+
+  // Get string length
+  size_t stringLength = strlen(fileBuf);
+  
+  // allocate device memory
+  char* d_fileBuf;
+  CUDA_SAFE_CALL(cudaMalloc((void**) &d_fileBuf, stringLength));
+  
+  // setup execution parameters
+  dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+  dim3 grid(WC / threads.x, HC / threads.y);
+
+  // execute the kernel
+  rot13Kern<<< grid, threads >>>(d_fileBuf, stringLength);
+
+  // check if kernel execution generated and error
+  CUT_CHECK_ERROR("Kernel execution failed");
+
+  // copy result from device to host
+  CUDA_SAFE_CALL(cudaMemcpy(h_C, d_C, mem_size_C,
+  						  cudaMemcpyDeviceToHost) );
+
+}
