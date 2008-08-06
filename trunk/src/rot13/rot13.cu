600e33f84b711c04591c4177e067a01de6cf2a19
jake.bower <jake.bower@4f06683a-ac53-0410-8745-075dec9ad3e4>
2008-08-06 21:03:05 +0000

    Rot 13 testing

--- trunk/src/rot13/rot13.cu
+++ trunk/src/rot13/rot13.cu
@@ -11,26 +11,26 @@
 #include <math.h>
 
 // includes, project
-//#include <cutil.h>
+#include <cutil.h>
 
 // includes, kernels
 #include <rot13Kern.cu>
 
-void rot13WholeFile(char* filename);
+void rot13OneLine(const char* filename);
 
 int
 main(int argc, char** argv)
 {
-    // Read file from disk to memory
-	if(argc < 2)
-	{
-	  printf("Usage: rot13 inputfile.txt");
-	  CUT_EXIT(argc, argv);
-	}
-	
-	rot13OneLine(argv[1]);
-	
+  // Read file from disk to memory
+  if(argc < 2)
+  {
+    printf("Usage: rot13 inputfile.txt");
     CUT_EXIT(argc, argv);
+  }
+	
+  rot13OneLine(argv[1]);
+	
+  CUT_EXIT(argc, argv);
 }
 
 /* This function assumes file is less than 10K and all one line */
@@ -38,7 +38,7 @@ void rot13OneLine(const char* filename)
 {
   const size_t bufferSz = 10240;
   // Allocate host memory
-  char* fileBuf = malloc(bufferSz);
+  char* fileBuf = (char*)malloc(bufferSz);
   
   // Read file
   FILE * h_file = fopen(filename, "r");
@@ -60,7 +60,7 @@ void rot13OneLine(const char* filename)
   // setup execution parameters
   dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
   dim3 grid(stringLength / threads.x, stringLength / threads.y);
-
+  printf("%s\n", fileBuf);
   // execute the kernel
   rot13Kern<<< grid, threads >>>(d_fileBuf, stringLength);
 
@@ -70,5 +70,7 @@ void rot13OneLine(const char* filename)
   // copy result from device to host
   CUDA_SAFE_CALL(cudaMemcpy(fileBuf, d_fileBuf, stringLength,
   						  cudaMemcpyDeviceToHost) );
+
+  printf("%s", fileBuf);
 
 }
--- trunk/src/rot13/rot13Kern.cu
+++ trunk/src/rot13/rot13Kern.cu
@@ -6,33 +6,43 @@
  
  #ifndef ROT13KERN_CU
  #define ROT13KERN_CU
+
+#define BLOCK_SIZE 16
  
  __global__ void
 rot13Kern( char* dataBuf, size_t length)
 {
   // Block index
-  int bx = blockIdx.x;
-  int by = blockIdx.y;
+  //int bx = blockIdx.x;
+  //int by = blockIdx.y;
 
   // Thread index
   int tx = threadIdx.x;
-  int ty = threadIdx.y;
+  //int ty = threadIdx.y;
 
   // control vars
-  int begin = 0;
-  int end = length - 1;
-  int step = BLOCK_SIZE;
+  //int begin = 0;
+  //int end = length - 1;
+  //int step = BLOCK_SIZE;
   
   __shared__ char dat[BLOCK_SIZE];
   
 //  for(int i = begin; i <= length; i += step)
 //  {
     // load the shared memory
-    dat[tx] = dataBuf[tx]
+    dat[tx] = dataBuf[tx];
     
     __syncthreads();
-    
-    dat[tx] = dat[tx] > 90 ? ((dat[tx] + 13) % 122 ) + 97 : ((dat[tx] + 13) % 90) + 65; 
+    if(dat[tx] > 90)
+    {
+      dat[tx] = /*((dat[tx] - 84) % 26 ) + 97*/ dat[tx] + 13;
+    }
+    else
+    { 
+      dat[tx] = ((dat[tx] - 52) % 26 ) + 65;
+    }
+    //dat[tx] = dat[tx] > 90 ? ((dat[tx] + 13) % 122 ) + 97 : ((dat[tx] 
+    //+ 13) % 90) + 65; 
  
     __syncthreads();
 //  }
